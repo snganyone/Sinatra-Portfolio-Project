66371ad243d04f3c175168d4ec0c0086cbc3f83f
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-27 21:27:05 -0700

    Views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,10 +6,13 @@ class JobsController < ApplicationController
         erb :'jobs/index'
     end
 
+    #create action
+
     #show action
 
-    get 'jobs/:id' do 
+    get '/jobs/:id' do 
         @jobs = Jobs.find_by_id(params[:id])
         erb :'jobs/show'
     end
+
 end
