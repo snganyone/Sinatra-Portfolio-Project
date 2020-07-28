8e80eea851b5ac8fd118eec4e219cadd596be7c5
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-28 13:40:16 -0700

    User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
+    #index action
     get '/users' do 
         erb :'users/index'
     end
 
-    get '/login' do 
-        erb :'users/login'
-    end
-
+    #User Signup
     get '/signup' do
         erb :'users/signup'
     end
+
+    post '/signup' do 
+    end
+
+    #User login
+
+    get '/login' do 
+        erb :'users/login'
+    end
+    
+    
 end
