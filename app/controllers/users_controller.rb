a206827399a7b1096c613ff3fd2a40a76f7613db
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-28 13:33:31 -0700

    Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
     get '/users' do 
         erb :'users/index'
     end
+
+    get '/login' do 
+        erb :'users/login'
+    end
+
+    get 'signup' do
+        erb :'users/signup'
+    end
 end
