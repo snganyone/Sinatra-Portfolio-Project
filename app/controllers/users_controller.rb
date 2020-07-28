d40006d2ded1603d88ea29dbc29bbe32dcdf9d70
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-28 14:49:01 -0700

    User Log In

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     #index action
     get '/users' do 
+        #@user = User.find(session[:user_id])
         erb :'users/index'
     end
 
@@ -12,13 +13,25 @@ class UsersController < ApplicationController
     post '/signup' do 
         @user = User.new(username: params["username"], email: params["email"], password: params["password"])
         @user.save
+
+        session[:user_id] = @user.id
         redirect '/users'
     end
 
     #User login
 
-    get '/login' do 
+    get '/users/login' do 
         erb :'users/login'
+    end
+
+    post '/login' do 
+        @user = User.find_by(username: params["username"])
+        if @user
+            session[:user_id] = @user.id
+            redirect '/users'
+        else
+            redirect '/users/login'
+        end
     end
     
     
