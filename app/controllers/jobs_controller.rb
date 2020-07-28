04aa63947d5d80d8847b607e870e33a99707b5da
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-27 21:13:10 -0700

    Views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,8 @@
 class JobsController < ApplicationController
+
+    #index action
+    get '/jobs' do
+        @jobs = Jobs.all 
+        erb :'jobs/index'
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,2 @@
-class Users < ApplicationController
+class UsersController < ApplicationController
 end
