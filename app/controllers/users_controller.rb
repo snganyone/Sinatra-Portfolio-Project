30a40d3cf1f0d40a0347862d875e70fd940bd27a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-08-07 22:02:50 -0700

    User Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     end
 
     post '/login' do 
-        @user = User.find_by(username: params["username"])
+        @user = User.find_by(email: params["email"])
         
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
